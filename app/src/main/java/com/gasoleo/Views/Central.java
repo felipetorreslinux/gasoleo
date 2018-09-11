a45667f95f8059201b79513095689f18bf6cf56e
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-09-11 14:22:22 -0300

    app

--- app/src/main/java/com/gasoleo/Views/Central.java
+++ app/src/main/java/com/gasoleo/Views/Central.java
@@ -3,22 +3,30 @@ package com.gasoleo.Views;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.webkit.PermissionRequest;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.gasoleo.Models.Arquivo;
 import com.gasoleo.R;
 
+import java.io.File;
+
 public class Central extends AppCompatActivity {
+
+    static final int REQUEST_FILE_INPUT = 72;
 
     Activity activity;
     ProgressDialog progressDialog;
@@ -49,6 +57,15 @@ public class Central extends AppCompatActivity {
             public void onPermissionRequest(final PermissionRequest request) {
                 request.grant(request.getResources());
             }
+
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Escolha"), REQUEST_FILE_INPUT);
+                return false;
+            }
         });
 
         webview.setWebViewClient(new WebViewClient(){
@@ -62,6 +79,13 @@ public class Central extends AppCompatActivity {
                 progressDialog.dismiss();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_FILE_INPUT && resultCode == RESULT_OK && data != null && data.getData() != null ) {
+            File arquivo = new File(data.getData().getPath());
+        }
     }
 
     @Override
